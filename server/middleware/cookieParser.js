31afada10afa3a9198fe99b1b73041a0a45c65fb
Amanda Winkles <amandawinkles@gmail.com>
2020-06-25 16:37:42 -0500

    Complete Parsing Cookie

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,16 @@
 const parseCookies = (req, res, next) => {
+  //req.headers.cookie //req.cookie
+  var cookieObj = {};
+  var cookies = req.headers.cookie.split('; ');
+  //loop through cookies arr
+  for (var i = 0; i < cookies.length; i++) {
+    //obj keys
+    var cookieKeyValue = cookies[i].split('=');
+    cookieObj[cookieKeyValue[0]] = cookieKeyValue[1];
+  }
+  return cookieObj;
+  next();
 };
+
 
 module.exports = parseCookies;
